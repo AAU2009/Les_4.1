4ace103bdb984f03ecdcaf6c0444a529438a2290
Artem Uskov <artem.uskov@gmail.com>
2024-01-24 22:15:16 +0000

    Lesson 4 v.3

--- test_main.py
+++ test_main.py
@@ -2,10 +2,6 @@
 import math
 import random
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def test_greeting():
     """
     Напишите программу, которая выводит на экран приветствие.
